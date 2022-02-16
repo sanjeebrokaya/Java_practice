1c51936541c2381e0a41edd00ff48df4433efb24
sanjeeb <sanjeeb.r11@gmail.com>
2022-02-16 20:12:47 +0545

    changes

--- printTriangle.java
+++ printTriangle.java
@@ -1,28 +1,24 @@
 public class printTriangle {
     
     public static void main(String[] args) {
-        System.out.println("*******");
-        System.out.println("******");
-        System.out.println("*****");
-        System.out.println("****");
-        System.out.println("***");
-        System.out.println("**");
-        System.out.println("*");
-        System.out.println("___________________");
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("*****");
-        System.out.println("******");
-        System.out.println("*******");
-        System.out.println("*******");
 
+        for(int row=0;row<=5;row++){
+            for (int col= 0; col <=row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
+        System.out.println("_________________\n");
 
+    // prnting inverted right angled triangle
 
-
-
-
+        for(int row=5;row>0;row--){
+            for (int col=0; col<row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
     }
 }
